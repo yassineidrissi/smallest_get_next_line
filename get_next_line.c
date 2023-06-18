d3b57d79be7fd660c05e9d880ca653a6ab890cae
Yassine Idrissi <yaidriss@c2r3p4.1337.ma>
2023-06-18 18:34:45 +0100

    it work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 18:42:49 by yaidriss          #+#    #+#             */
-/*   Updated: 2023/06/18 17:42:47 by yaidriss         ###   ########.fr       */
+/*   Updated: 2023/06/18 18:34:04 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,25 @@ char   *get_next_line(int fd)
     int j = -1;
     int rd;
     char *line;
-    static  char *str;
-    str = malloc(99999);
+    static  char str[99999] = {0};
 
-    rd = read(fd, str, BUFFER_SIZE - BUFFER_SIZE + 1);
-    if (rd <= 0)
-    {
-        free(str);
-        return NULL;
-    }
+    rd = read(fd, str, 1);
+    if (rd <= 0 || BUFFER_SIZE < 1)
+        return 0;
     while (rd && str[i - 1] != '\n')
     {
         i += rd;
         if (str[0] == '\n')
             break;
-        rd = read(fd, str + i, BUFFER_SIZE - BUFFER_SIZE + 1);
+        rd = read(fd, str + i, 1);
         if (str[i] == '\n')
             i++;
     }
     line = ft_calloc(i + 1, 1);
+    //! in the exam u can samply change it by malloc 
+    //!ther is no need to add get_next_line_utils file
     while(++j < i && str[j])
        line[j] = str[j];
     line[j] = 0;
-    free(str);
-    str = 0;
     return (line);  
 }
